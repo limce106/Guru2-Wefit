53e49ada8abf3564ece3f932d030e5dba93060d7
so00ee <moonsy2559@gmail.com>
2023-01-30 16:49:46 +0900

    수정중

---
+++ app/src/main/java/com/example/guru2/firstLogo.kt
@@ -0,0 +1,13 @@
+package com.example.guru2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class firstLogo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_first_logo)
+
+
+    }
+}
--- app/src/main/java/com/example/guru2/myPage.kt
+++ app/src/main/java/com/example/guru2/myPage.kt
@@ -1,16 +1,21 @@
 package com.example.guru2
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.UserInfo
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.*
 import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
+import org.checkerframework.checker.guieffect.qual.UI
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -26,13 +31,18 @@ private const val ARG_PARAM2 = "param2"
  */
 class myPage : Fragment() {
 
+    var list:ArrayList<User> = ArrayList()
     var mAuth = FirebaseAuth.getInstance()
     var mDbRef:DatabaseReference = FirebaseDatabase.getInstance().reference
+    var user=FirebaseAuth.getInstance().currentUser
+    var userId= user?.uid
+    var adapter: UserAdapter = UserAdapter(requireActivity(), list)
+
+
 
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-
 
 
 
@@ -53,27 +63,41 @@ class myPage : Fragment() {
 
 
 
-        val page_name : TextView = view.findViewById(R.id.page_name)
+        var page_name : TextView = view.findViewById(R.id.page_name)
         val page_id : TextView = view.findViewById(R.id.page_id)
         val page_gender : TextView = view.findViewById(R.id.page_gender)
         val page_purpose : TextView = view.findViewById(R.id.page_purpose)
 
+        mDbRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+
+                val userInfo=dataSnapshot.getValue(User::class.java)
+                val key=dataSnapshot.key
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.w(TAG, "Failed to read value.", error.toException())
+            }
+        })
+        // [END read_message]
 
 
-        mDbRef.child("user")
-            .addValueEventListener(object: ValueEventListener {
-                override fun onDataChange(snapshot: DataSnapshot) {
 
-
-                      //  page_name=mAuth.getValue(User::class.java)
-
-
-                }
-
-                override fun onCancelled(error: DatabaseError) {
-                }
-
-            })
+//        mDbRef.child("user")
+//            .addValueEventListener(object: ValueEventListener {
+//                override fun onDataChange(snapshot: DataSnapshot) {
+//
+//
+//                      //  page_name=mAuth.getValue(User::class.java)
+//
+//
+////                }
+////
+////                override fun onCancelled(error: DatabaseError) {
+////                }
+//
+//            })
 
 
 
