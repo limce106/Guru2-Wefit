68238ef95fc6008595abf8bf3a4d96a85342b925
채은 임 <limce106@naver.com>
2023-01-30 18:57:09 +0900

    강사 운동/식단 기록 어댑터 연결

--- app/src/main/java/com/example/guru2/Records/RecordMain.kt
+++ app/src/main/java/com/example/guru2/Records/RecordMain.kt
@@ -54,6 +54,4 @@ class RecordMain : Fragment() {
 
         return view
     }
-
-
 }
--- app/src/main/java/instructure_record/Instructure_Record_Main.kt
+++ app/src/main/java/instructure_record/Instructure_Record_Main.kt
@@ -9,10 +9,13 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.FrameLayout
 import android.widget.Toast
 import androidx.annotation.NonNull
 import androidx.fragment.app.Fragment
+import com.example.guru2.NaviActivity
 import com.example.guru2.R
+import com.google.android.material.tabs.TabLayout
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
@@ -33,15 +36,37 @@ class Instructure_Record_Main : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val rootView = inflater.inflate(R.layout.fragment_instructure__record_main, container, false)
+        val view = inflater.inflate(R.layout.fragment_instructure_record_main, container, false)
+        val tabLayout = view.findViewById<TabLayout>(R.id.tab_record) // 탭 레이아웃
+        val recordframe = view.findViewById<FrameLayout>(R.id.recordframe)
+        val mActivity = activity as NaviActivity
+        mActivity.supportFragmentManager.beginTransaction().add(R.id.recordframe,
+            Instructure_Exercise_Fragment()).commit()
+
+        //탭이 선택되었을 때 페이지가 바꾸도록 함
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                if(tab!!.position == 0){
+                    mActivity.changeTab(Instructure_Exercise_Fragment())
+                } else if(tab.position == 1){
+                    mActivity.changeTab(Instructure_Meal_Fragment())
+                }
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+            }
+        })
 
         // 닉네임 입력 변화 확인
-        var edtNickName = rootView.findViewById<EditText>(R.id.edtNickName)
+        var edtNickName = view.findViewById<EditText>(R.id.edtNickName)
         checkChanges(edtNickName)
 
         val database = FirebaseDatabase.getInstance()
         val databaseReference = database.getReference("user") // DB 테이블 연결
-        val btnOk = rootView.findViewById<Button>(R.id.btnNicknameOk)
+        val btnOk = view.findViewById<Button>(R.id.btnNicknameOk)
         btnOk.setOnClickListener() {
             databaseReference.orderByChild("reg_id").equalTo(strNickname)
                 .addListenerForSingleValueEvent(object : ValueEventListener {
@@ -66,7 +91,7 @@ class Instructure_Record_Main : Fragment() {
                 })
         }
 
-        return rootView
+        return view
     }
 
     fun checkChanges(edtNickname: EditText) {
