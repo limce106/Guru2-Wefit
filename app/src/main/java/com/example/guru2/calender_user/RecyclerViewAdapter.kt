ee3de2cf34f35f846aa8e193012ccfca0bd884a0
Sohyun <so405@naver.com>
2023-01-31 16:57:37 +0900

    Revert "일정 관리 삭제 수정, 강사 운동 추천 수"
    This reverts commit c2156c2720290dee8083edaad4ecc8e5421129f2.

--- app/src/main/java/com/example/guru2/calender_user/RecyclerViewAdapter.kt
+++ app/src/main/java/com/example/guru2/calender_user/RecyclerViewAdapter.kt
@@ -1,51 +1,37 @@
 package com.example.guru2.calender_user
 
-import android.annotation.SuppressLint
-import android.app.AlertDialog
-import android.content.Context
-import android.content.DialogInterface
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
-import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
+import com.example.guru2.NaviActivity
 import com.example.guru2.R
-import com.example.guru2.Records.RecyclerAdapterMeal2
-import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 
 
-class RecyclerViewAdapter():RecyclerView.Adapter<RecyclerViewAdapter.RecyclerViewHolder>(){
-    private lateinit var itemList: ArrayList<Schedule>
-    lateinit var ct: Context
-    private lateinit var uidList: ArrayList<String>
+class RecyclerViewAdapter(val itemList: ArrayList<Schedule>, context:android.content.Context,uidList: ArrayList<String>):RecyclerView.Adapter<RecyclerViewAdapter.RecyclerViewHolder>(){
 
-    constructor(itemList: ArrayList<Schedule>, context: android.content.Context, uidList: ArrayList<String>) : this() {
-        this.itemList = itemList
-        ct = context
-        this.uidList = uidList
-    }
 
+    private val firebaseDatabase: FirebaseDatabase = FirebaseDatabase.getInstance()
+    val mActivity = context as NaviActivity
+    val databaseReference: DatabaseReference = firebaseDatabase.getReference("schedule").child(mActivity.loginUser()!!) //db 연결
+    val ct = context
+    var uidList:ArrayList<String> = uidList
 
     //아이템 레이아웃과 결합
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.calender_item,parent,false)
-        val holder: RecyclerViewAdapter.RecyclerViewHolder = RecyclerViewAdapter.RecyclerViewHolder(view)
-        return holder
+        return RecyclerViewHolder(view)
     }
 
     //view에 내용 입력
     override fun onBindViewHolder(holder: RecyclerViewHolder, position: Int) {
-        holder.bind(itemList[position])
-
-        holder.iv_delete2.setOnClickListener(){
-            removeData(position)
-            Log.e("삭제", position.toString())
-        }
+        holder.time.text = itemList[position].time
+        holder.type.text = itemList[position].type
+        holder.date.text = itemList[position].date
     }
 
     //리스트 내 아이템 개수
@@ -55,51 +41,23 @@ class RecyclerViewAdapter():RecyclerView.Adapter<RecyclerViewAdapter.RecyclerVie
 
 
     //레이아웃 내 View 연결
-    class RecyclerViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
-        val iv_delete2: ImageView = itemView.findViewById(R.id.iv_delete2)
-
-        fun bind(item: Schedule)
-        {
-            val time = itemView.findViewById<TextView>(R.id.time)
-            val type = itemView.findViewById<TextView>(R.id.schedule_type)
-            val date = itemView.findViewById<TextView>(R.id.calender_date)
-        }
+    inner class RecyclerViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
+        val time = itemView.findViewById<TextView>(R.id.time)
+        val type = itemView.findViewById<TextView>(R.id.schedule_type)
+        val date = itemView.findViewById<TextView>(R.id.calender_date)
     }
-
-    // 리스너 인터페이스
-    interface OnItemClickListener {
-        fun onClick(v: View, position: Int)
-    }
-    // 외부에서 클릭 시 이벤트 설정
-    fun setItemClickListener(onItemClickListener: OnItemClickListener) {
-        this.scheduleClickListener = onItemClickListener
-    }
-    // setItemClickListener로 설정한 함수 실행
-    private lateinit var scheduleClickListener : OnItemClickListener
 
     //데이터 삭제 함수
     fun removeData(position: Int){
-        val database = FirebaseDatabase.getInstance()
-        var user= FirebaseAuth.getInstance().currentUser
-        var userId= user?.uid
-        val myRef = database.getReference("schedule").child(userId!!)
-        val builder = AlertDialog.Builder(ct)
-        builder.setTitle("삭제")
-            .setMessage("해당 항목을 삭제하시겠습니까?")
-            .setPositiveButton("확인",
-                DialogInterface.OnClickListener{ dialog, id ->
-                    myRef.child(uidList[position]).removeValue().addOnSuccessListener {
-                        Toast.makeText(ct, "삭제 완료", Toast.LENGTH_SHORT).show() }
-                    itemList.removeAt(position)
-                    notifyItemRemoved(position)
-                    notifyItemRangeChanged(position, itemList.size)
-                })
-            .setNegativeButton("취소",
-                DialogInterface.OnClickListener{ dialog, id ->
-                    dialog.cancel()
-                })
 
-        // 다이얼로그 띄우기
-        builder.show()
+        databaseReference.child(uidList[position]).removeValue().addOnSuccessListener {
+            Toast.makeText(ct, "삭제 완료", Toast.LENGTH_SHORT).show() }
+             itemList.removeAt(position)
+            notifyItemRemoved(position)//특정 아이템 1개 삭제
+            notifyItemRangeChanged(position, itemList.size)
+
     }
+
+
+
 }
